53659f77a69ce88073b03948e6df38f56876fcc7
Matt Zamora <mzamora2899@gmail.com>
2023-02-12 15:33:51 -0700

    repo setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Node Guardians</title>
+                <meta name="description" content="Node Guardians frontend" />
+            </Head>
+
+            <main>
+                <h1>Home Page</h1>
+            </main>
+        </>
+    )
+}
---
+++ pages/quests/[id].tsx
@@ -0,0 +1,3 @@
+export default function SingleQuest() {
+    return <div>Single Quest</div>
+}
